9836db09f29148b1849b9a9811020ec489c65d07
prz3m0k0x <przemekgolasz@gmail.com>
2026-03-26 14:28:22 +0100

    newest version for optislang runfile

--- so2/run_optimization.py
+++ so2/run_optimization.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 import subprocess, os
 
-so3_output = 0.0
-so2_output = 0.0
-
 def extract_last_value(filepath):
     if os.path.exists(filepath):
         with open(filepath, "r") as f:
@@ -11,27 +8,25 @@ def extract_last_value(filepath):
                     
             valid_lines = [line for line in lines if line.strip()]
             if valid_lines:
-                last_line = valid_lines[-1]
-                       
+                last_line = valid_lines[-1]  
                 parts = last_line.split()
-                if len(parts) >= 2:
-                    return float(parts[1])
+                return float(parts[1])
                         
             print(f"Warning: Could not read {filepath}")
             return 0.0
 
 def run(LengthCat1, LengthCool, LengthCat2, inlet_Y_SO2, path, script_file, mixture_file, solver_file):
     py = r"C:\Users\praktyka\AppData\Local\Programs\Python\Python313\python.exe"
-    script = r"C:\Users\praktyka\Desktop\Golasz\PyFluent\pyfluent\so2\solution.py"
+    script = r"C:\Users\praktyka\Desktop\Golasz\optislang\test2\test2.opr\solution.py"
     args = [str(LengthCat1), str(LengthCool), str(LengthCat2), str(inlet_Y_SO2), str(path), str(script_file), str(mixture_file), str(solver_file)]
     
 
     subprocess.run([py, script, *args], check=True, cwd= path)
 
-    extracted_so3 = extract_last_value(os.path.join(path, "so3-outlet-fraction.out"))
-    extracted_so2 = extract_last_value(os.path.join(path, "so2-outlet-fraction.out"))
+    extracted_so3 = float(extract_last_value(os.path.join(path, "so3-outlet-fraction.out")))
+    extracted_so2 = float(extract_last_value(os.path.join(path, "so2-outlet-fraction.out")))
 
-    return float(extracted_so3), float(extracted_so2)
+    return {"so3_fraction" : extracted_so3, "so2_fraction" : extracted_so2}
  
 path= Path(DESIGN_DIR)
 
@@ -58,7 +53,7 @@ if opr_dir:
 else:
     print("Error: Could not locate the .opr directory in any parent folders.")
 
-raw_so3,raw_so2 = run(LengthCat1, LengthCool, LengthCat2, inlet_Y_SO2, path, script_file, mixture_file, solver_file)
+output = run(LengthCat1, LengthCool, LengthCat2, inlet_Y_SO2, path, script_file, mixture_file, solver_file)
 
-so3_output = float(raw_so3)
-so2_output = float(raw_so2)
+so3_fraction = real(output["so3_fraction"])
+so2_fraction = real(output["so2_fraction"])
